209f872c02c4b71e1df48486de531517874a08ba
Suele Guimarães <suelen.ads.guimaraes@gmail.com>
2020-03-10 02:30:57 -0300

    Add class Stack and change name files.

---
+++ stack/Stack.java
@@ -0,0 +1,9 @@
+public class Stack{
+
+    private String nomes[] = new String[4];
+    private Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
