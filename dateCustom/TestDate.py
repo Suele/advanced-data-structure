de4e4015ec156d49eb5849e627a74f698186d2eb
suele <suelen.ads.guimaraes@gmail.com>
2020-04-08 05:01:44 -0300

    Change name class TestDate.

--- dateCustom/TestDate.py
+++ dateCustom/TestDate.py
@@ -1,4 +1,5 @@
-from dateStruct import DateStruct
+from DateStruct import DateStruct
+from Controller import Controller
 
 day = input("Digite o dia desejado: ")
 month = input("Digite o mês desejado: ")
@@ -8,8 +9,26 @@ print(" ")
 
 try:
     date = DateStruct(int(day), int(month), int(year))
-    date.anoBissexto()
-    date.verificaData()
-    date.dataDaPascoa()
+    controller = Controller(date)
+    controller.anoBissexto()
+    controller.verificaData()
+    controller.dataDaPascoa()
+
+    print(" ")
+
+    day = input("Digite o dia desejado: ")
+    month = input("Digite o mês desejado: ")
+    year = input("Digite o ano desejado: ")
+    date1 = DateStruct(int(day), int(month), int(year))
+
+    print(" ")
+
+    day = input("Digite o dia desejado: ")
+    month = input("Digite o mês desejado: ")
+    year = input("Digite o ano desejado: ")
+    date2 = DateStruct(int(day), int(month), int(year))
+    controller.datasIguais(date1, date2)
+
 except:
     print("Algum ou todos os dados inseridos não são números")
+
